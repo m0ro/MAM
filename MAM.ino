4eb70cbdbdb11488fc59d2055ccbd9c73e333d9c
m0ro <ilbaffomoretti@gmail.com>
2021-04-14 13:50:54 +0200

    Update MAM.ino
    large updates. better error/hardware handling. still i2c not working (probably hardware issue)

--- MAM.ino
+++ MAM.ino
@@ -17,17 +17,19 @@ remember to press "boot" button to updoad the firmware
 implement timers and sleep mode? (for battery operation)
 https://randomnerdtutorials.com/esp32-data-logging-temperature-to-microsd-card/
 
-per qualche motivo l'SD si inizializza, vede la dimensione, ma poi non fa altro.
-togliendo la parte di codice dell'SD parte normalmente, ma pare solo dopo un po' di tempo
-senza boot, o dopo un paio di boot a vuoto...strano...che sia un problema di formattazione dell'SD?
-provare con degli esempi su un'altra board, usando per esempio il pinout "standard"
-**********************************************************************/
-
 // MCU: ESPRESSIF ESP32-WROOM-32D
-// particulate sensor: PMS7003 
+// dust sensor: PMS7003 
 // CO2 sensor: MH-Z19B
 // Humidity/Temperature sensor: HTU21
 // Display: SPI oled display 128x64
+
+**********************************************************************/
+
+/****
+ * di fatto l'unica roba che non funziona e' l'i2c...sembra non essere reliable, ogni tanto va, ogni tanto no...un contatto falso nel socket? controllare se si perdono bit (e.g. fare un confronto dei
+ * dati prima e dopo con l'oscilloscopio, tipo una funzione "sottrazione"...
+ *****/
+
 
 #include <Arduino.h>
 #include <SPI.h>
@@ -67,12 +69,13 @@ char password[50];
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define SCREEN_SDA 21
-#define SCREEN_SCL 22
+#define SCREEN_SDA 22
+#define SCREEN_SCL 21
 #define OLED_RESET -1 
 #define WIFI true
 
 char output[256];
+int timeout = 0;
 
 // use NTP server to get time
 const char* ntpServer = "pool.ntp.org";
@@ -80,6 +83,7 @@ const long  gmtOffset_sec = 3600;
 const int   daylightOffset_sec = 3600;
 struct tm timeinfo;
 
+// create objects for each sensor
 Plantower_PMS7003 dustsensor = Plantower_PMS7003();
 MHZ19 CO2sensor;                                             
 HardwareSerial CO2serial(0);
@@ -94,9 +98,8 @@ double hum;
 double temp;
 float CO2;
 
+// initialize webserver
 WebServer server(80);
-
-//// some functions to manage the sd card  ////
 
 void readFile(fs::FS &fs, const char * path){
     display.printf("Reading file: %s\n", path);
@@ -166,11 +169,8 @@ void appendFile(fs::FS &fs, const char * path, const char * message){
     file.close();
 }
 
-/////////////////////////////////
-
-
 // Handle root url (/)
-void handle_root() {
+void handle_root(){
   sprintf(output, 
   "<b>PM<sub>1.0</sub></b>:  %2d ug/m<sup>3</sup><br>"
   "<b>PM<sub>2.5</sub></b>:  %2d ug/m<sup>3</sup><br>"
@@ -179,148 +179,38 @@ void handle_root() {
   "<b>Temp.</b>:  %.1f &deg;C<br>"
   "<b>CO<sub>2</sub></b>  :  %.0f ppm<br>",
   pm1,pm25, pm10, hum, temp, CO2);
-  
   server.send(200, "text/html", output);
 }
 
-void setup()
-{
-
-  Wire.begin(SCREEN_SDA, SCREEN_SCL);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C, false, false)) {
-    display.println(F("SSD1306 allocation failed"));
-  }
-  // Clear the buffer
-  display.clearDisplay();
-  display.display();
-  // Normal 1:1 pixel scale
-  display.setTextSize(1);      
-  display.setTextColor(WHITE);
-  display.ssd1306_command(SSD1306_SETCONTRAST);
-  display.ssd1306_command(3);
-
-
-  // serial commumication through USB
-  Serial.begin(BAUDRATE);
-
-  // sd card initialization
-//  SPIClass sd_spi(VSPI);
-//  sd_spi.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
-  SD.begin(SD_CS); 
-//  if (!SD.begin(SD_CS, sd_spi)){
-  if (!SD.begin(SD_CS)){
-    display.println("Card Mount Failed");
-    return;
-  }
-  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-  display.printf("SD Size: %lluMB\n", cardSize);
-  // verify if log file exist, if not, create it
-  File file = SD.open("/pm_log.txt");
-  if(!file) {
-    display.println("pm_log doens't exist");
-    display.println("Creating pm_log...");
-    writeFile(SD, "/pm_log.txt", "date,pm1,pm25,pm10,hum,temp,CO2 \r\n");
-  }
-  else {
-    display.println("pm_log already exists");  
-  }
-  file.close();
-  
-  // read ssid and pw of wifi from wifi.txt in the sd card
-  readWifiPars(SD, "/wifi.txt");
-  display.println(ssid);
-  display.println(password);
-  
-  // serial with PMS7003
-  Serial2.begin(BAUDRATE);
-  dustsensor.init(&Serial2);
-  // serial with MHZ19
-  CO2serial.begin(BAUDRATE, SERIAL_8N1, CO2_TX_PIN, CO2_RX_PIN); // ESP32 example
-  CO2sensor.begin(CO2serial); 
-  
-  if (!htu.begin()) {
-    display.println("HTU21 allocation failed");
-    while (1);
-  }
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    display.print(".");
-    display.display();
-  }
-  
-  display.println("");
-  display.println(ssid);
-  display.println(WiFi.localIP());
-  display.display();
-  if (MDNS.begin("esp32")) {
-    display.println("multicast DNS start");
-  }
-  server.on("/", handle_root);
-  server.begin();
-  display.println("HTTP server started");
-
-  //init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  
-  display.display();
-  
-  // wait a little to initialize stuff
-  delay(2000);
-  
+void print_to_serial(){
+    // print to serial
+    Serial.println(&timeinfo, "%H:%M:%S %d/%m/%y");
+    sprintf(output, "PM1.0: %2d ug/m3\nPM2.5: %2d ug/m3\nPM10 : %2d ug/m3",pm1,pm25,pm10);
+    Serial.println(output);
+    sprintf(output, "Hum. :  %.1f %c\nTemp.:  %.1f %cC", hum,(char)37,temp,(char)247);
+    Serial.println(output);
+    sprintf(output, "CO2  :  %.0f ppm", CO2);
+    Serial.println(output);
+    Serial.println(WiFi.localIP());
 }
 
-void loop()
-{
-  server.handleClient();
-  dustsensor.updateFrame();
-  if (dustsensor.hasNewData()) {
-    
+void print_to_screen(){
+    // print to screen
     display.clearDisplay();
     display.setCursor(0, 0);
-
-    // could be interesting to set the display on only between certain times...
-    // or use a free pin for a switch...or read from web, or from SD
-//    display.ssd1306_command(SSD1306_DISPLAYON);
-//    display.ssd1306_command(SSD1306_DISPLAYOFF);
-    
-    // get time and display it
-    if(!getLocalTime(&timeinfo)){
-      display.println("fail to get time");
-    }
-    strftime(formatted_date, sizeof(formatted_date), "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
     display.println(&timeinfo, "%H:%M:%S %d/%m/%y");
-
-    // dust sensor
-    // all in (ug/m3)
-    pm1 = dustsensor.getPM_1_0_atmos();
-    pm25 = dustsensor.getPM_2_5_atmos();
-    pm10 = dustsensor.getPM_10_0_atmos();
     sprintf(output, "PM1.0: %2d ug/m3\nPM2.5: %2d ug/m3\nPM10 : %2d ug/m3",pm1,pm25,pm10);
     display.println(output);
-    
-    // humidity / temperature sensor
-    temp = htu.readTemperature();
-    hum = htu.readHumidity();
     sprintf(output, "Hum. :  %.1f %c\nTemp.:  %.1f %cC", hum,(char)37,temp,(char)247);
     display.println(output);
-
-    // CO2 sensor / double?
-    CO2 = CO2sensor.getCO2Raw();
-    int8_t CO2temp = CO2sensor.getTemperature();  
-    // Exponential equation for Raw & CO2 relationship (see docs)
-    CO2 = 6.60435861e+15 * exp(-8.78661228e-04 * CO2);
     sprintf(output, "CO2  :  %.0f ppm", CO2);
     display.println(output);
-
-    // display the current IP
     display.println(WiFi.localIP());
-
     display.display();
+}
 
+void dump_to_sd(){
+    // dump to sd card
     dataMessage = String(formatted_date) + ","  
                 + String(pm1) + "," 
                 + String(pm25) + "," 
@@ -329,6 +219,137 @@ void loop()
                 + String(temp) + "," 
                 + String(CO2) + "\r\n";
     appendFile(SD, "/pm_log.txt", dataMessage.c_str());
+}
+
+void setup(){
+  // serial commumication through USB
+  Serial.begin(BAUDRATE);
+  Serial.println('Serial active...');
+//  Wire.begin(SCREEN_SDA, SCREEN_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C, false, false)) {
+    Serial.println("SSD1306 allocation failed");
+  }
+  else {
+    Serial.println("SSD1306 allocation succeed");
+    // Clear the buffer
+    display.clearDisplay();
+    display.display();
+    // Normal 1:1 pixel scale
+    display.setTextSize(1);      
+    display.setTextColor(WHITE);
+    display.ssd1306_command(SSD1306_SETCONTRAST);
+    display.ssd1306_command(50);
+    display.println("SSD1306 allocation succeed");
+    display.display();
+  }
+  if (!htu.begin()) {
+    Serial.println("HTU21 allocation failed");
+  }
+  else{
+    Serial.println("HTU21 allocation succeed");
+  }
+  // sd card initialization
+//  SPIClass sd_spi(VSPI);
+//  sd_spi.begin(SD_SCK, SD_MISO, SD_MOSI, SD_CS);
+  SD.begin(SD_CS); 
+//  if (!SD.begin(SD_CS, sd_spi)){
+  if (!SD.begin(SD_CS)){
+    Serial.println("Card Mount Failed");
+//    return;
+  }
+  else{
+    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+    Serial.printf("SD Size: %lluMB\n", cardSize);
+    // verify if log file exist, if not, create it
+    File file = SD.open("/pm_log.txt");
+    if(!file) {
+      Serial.println("pm_log doens't exist");
+      Serial.println("Creating pm_log...");
+      writeFile(SD, "/pm_log.txt", "date,pm1,pm25,pm10,hum,temp,CO2 \r\n");
+    }
+    else{
+      Serial.println("pm_log already exists");  
+    }
+    file.close();
+    
+    // read ssid and pw of wifi from wifi.txt in the sd card
+    readWifiPars(SD, "/wifi.txt");
+    Serial.println(ssid);
+    Serial.println(password);
+  }
+  
+  // start serial for PMS7003
+  Serial2.begin(BAUDRATE);
+  dustsensor.init(&Serial2);
+  // start serial for MHZ19
+  CO2serial.begin(BAUDRATE, SERIAL_8N1, CO2_TX_PIN, CO2_RX_PIN);
+  CO2sensor.begin(CO2serial); 
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  timeout = 0;
+  while ((WiFi.status() != WL_CONNECTED) and (timeout<10)) {
+    delay(500);
+    Serial.print(".");
+    timeout = timeout + 1;
+  //    display.display();
+  }
+  if (timeout<10){
+    Serial.println("");
+    Serial.println(ssid);
+    Serial.println(WiFi.localIP());
+  //  display.display();
+    if (MDNS.begin("esp32")){
+      Serial.println("multicast DNS start");
+    }
+    server.on("/", handle_root);
+    server.begin();
+    Serial.println("HTTP server started");
+  
+    //init and get the time
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  }
+  
+  display.display();
+  
+  // wait a little to initialize stuff
+  delay(2000);
+  
+}
+
+void loop(){
+  server.handleClient();
+  dustsensor.updateFrame();
+  // could be interesting to set the display on only between certain times...
+  // or use a free pin for a switch...or read from web, or from SD
+  display.ssd1306_command(SSD1306_DISPLAYON);
+  //    display.ssd1306_command(SSD1306_DISPLAYOFF);
+
+  // it's really needed to activate only if there is new data from dustsensor?
+  if (dustsensor.hasNewData()){
+    // collect data
+    // dust sensor, all in (ug/m3)
+    pm1 = dustsensor.getPM_1_0_atmos();
+    pm25 = dustsensor.getPM_2_5_atmos();
+    pm10 = dustsensor.getPM_10_0_atmos();
+    // humidity / temperature sensor
+    temp = htu.readTemperature();
+    hum = htu.readHumidity();
+    // CO2 sensor
+    CO2 = CO2sensor.getCO2Raw();
+    int8_t CO2temp = CO2sensor.getTemperature();  
+    // Exponential equation for Raw & CO2 relationship (see docs)
+    CO2 = 6.60435861e+15 * exp(-8.78661228e-04 * CO2);
+    // get time and format it
+    if(!getLocalTime(&timeinfo)){
+      Serial.println("fail to get time");
+    }
+    strftime(formatted_date, sizeof(formatted_date), "%Y-%m-%dT%H:%M:%SZ", &timeinfo);
+
+    print_to_serial();
+    print_to_screen();
+    dump_to_sd();
 
     delay(5*1000);
   }
